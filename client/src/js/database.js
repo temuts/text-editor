32b9c62d79c1da47af9bc5cf20f15240de192211
Temu Ts <tem.tsengel@utoronto.ca>
2023-10-15 16:51:19 -0400

    updated putDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,14 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Storing data in the database');
+  const myDatabase = await openDB('myDatabase', 1);
+  const transaction = myDatabase.transaction('myCollection', 'readwrite');
+  const objectStore = transaction.objectStore('myCollection');
+  const putRequest = objectStore.put({ id: 1, value: content });
+  const result = await putRequest;
+  console.error('putDb not implemented')};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
